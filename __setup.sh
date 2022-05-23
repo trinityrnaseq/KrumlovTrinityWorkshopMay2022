1bf9616c94761d524a27446a66ff152ee1435fa0
Brian Haas <brianjohnhaas@gmail.com>
2022-05-23 09:12:03 -0400

    update

--- __setup.sh
+++ __setup.sh
@@ -7,4 +7,10 @@ cd data/
 wget https://data.broadinstitute.org/Trinity/RNASEQ_WORKSHOP/trinotate_data.Krumlov2019.tar.gz
 
 tar xvf trinotate_data.Krumlov2019.tar.gz
+rm -f trinotate_data.Krumlov2019.tar.gz
 
+for gz_file in *.gz; do gunzip -c $gz_file > ${gz_file%.*}; done
+
+set +e
+
+echo done
